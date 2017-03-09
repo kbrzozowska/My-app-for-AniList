05730f40e8703b89213565c9ef65cbf64da32d91
Kasia Brzozowska <k.d.brzozowska@gmail.com>
2017-03-09 11:32:43 +0100

    authentication problem

--- js/app.jsx
+++ js/app.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import cacheProxy from './cacheProxy'
+import {tokenStorage} from 'fetch-oauth2';
+import {fetchWithMiddleware, middleware} from 'fetch-oauth2';
 import {Router, Route, Link, IndexRoute, hashHistory} from 'react-router';
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    // const storage = tokenStorage({initialToken, fetchToken, generateToken});
+    // const oauth2Fetch = fetchWithMiddleware(middleware.authorisationChallengeHandler(storage), middleware.setOAuth2Authorization(storage));
+    //
+    // oauthFetch('https://anilist.co/api/auth/authorize?grant_type=client_credentials&client_id=brethil-sc5w1&response_type=code&redirect_uri=Array').then(response => resp.json()).catch(error => {
+    //     console.log(err);
+    //
+    // });
+
     class AnimeList extends React.Component {
         constructor(props) {
             super(props);
-
             this.state = {
-                access_token: "",
                 loading: true
             };
         }
         aniList() {
 
-            fetch('https://anilist.co/auth/access_token', {
+            fetch('https://anilist.co/api/auth/authorize?grant_type=client_credentials&client_id=brethil-sc5w1&response_type=code&redirect_uri=Array', {
                 method: 'POST',
                 headers: {
                     grant_type: "client_credentials",
@@ -26,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             }).then(resp => resp.json()).then(data => {
                 console.log(data);
-                this.setState({access_token, loading: false});
+                this.setState({loading: false});
             });
 
             // const access = {
